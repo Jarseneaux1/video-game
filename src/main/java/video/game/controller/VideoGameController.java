2f7452cfcddf331f7a8fd9b55f6398e3e75fb411
Jarseneaux1 <joshuamarseneaux@gmail.com>
2023-11-04 12:05:53 -0400

    6th

--- src/main/java/video/game/controller/VideoGameController.java
+++ src/main/java/video/game/controller/VideoGameController.java
@@ -10,33 +10,33 @@ import video.game.entity.game;
 import video.game.service.GameService; // Correct the import
 
 @RestController
-@RequestMapping("/api/games")
+@RequestMapping("/api/games") //for ARC  
 public class VideoGameController {
     @Autowired
-    private GameService gameService; // Use the corrected import
+    private GameService gameService; 
 
     @PostMapping
-    public game createGame(@RequestBody game game) {
+    public game createGame(@RequestBody game game) { //Create  
         return gameService.createGame(game);
     }
 
     @GetMapping("/{gameId}")
-    public game getGameById(@PathVariable Long gameId) {
+    public game getGameById(@PathVariable Long gameId) { //Read  
         return gameService.getGameById(gameId);
     }
 
     @GetMapping
-    public List<game> getAllGames() {
+    public List<game> getAllGames() { //Read 
         return gameService.getAllGames();
     }
 
     @PutMapping
-    public game updateGame(@RequestBody game game) {
+    public game updateGame(@RequestBody game game) { //Update  
         return gameService.updateGame(game);
     }
 
     @DeleteMapping("/{gameId}")
-    public void deleteGame(@PathVariable Long gameId) {
+    public void deleteGame(@PathVariable Long gameId) { //Delete  
         gameService.deleteGame(gameId);
     }
 
