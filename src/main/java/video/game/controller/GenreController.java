92d50812bde36732aab412748d3134ef99b62d6e
Jarseneaux1 <joshuamarseneaux@gmail.com>
2023-11-04 12:05:36 -0400

    5th

--- src/main/java/video/game/controller/GenreController.java
+++ src/main/java/video/game/controller/GenreController.java
@@ -9,33 +9,33 @@ import video.game.entity.genre;
 import video.game.service.GenreService;
 
 @RestController
-@RequestMapping("/api/genres")
+@RequestMapping("/api/genres") //for ARC 
 public class GenreController {
     @Autowired
     private GenreService genreService;
 
     @PostMapping
-    public genre createGenre(@RequestBody genre genre) {
+    public genre createGenre(@RequestBody genre genre) { //Create 
         return genreService.createGenre(genre);
     }
 
     @GetMapping("/{genreId}")
-    public genre getGenreById(@PathVariable Long genreId) {
+    public genre getGenreById(@PathVariable Long genreId) { //Read 
         return genreService.getGenreById(genreId);
     }
 
     @GetMapping
-    public List<genre> getAllGenres() {
+    public List<genre> getAllGenres() { //Read 
         return genreService.getAllGenres();
     }
 
     @PutMapping
-    public genre updateGenre(@RequestBody genre genre) {
+    public genre updateGenre(@RequestBody genre genre) { //Update 
         return genreService.updateGenre(genre);
     }
 
     @DeleteMapping("/{genreId}")
-    public void deleteGenre(@PathVariable Long genreId) {
+    public void deleteGenre(@PathVariable Long genreId) { //Delete 
         genreService.deleteGenre(genreId);
     }
 
